e452624135527ed7c7896321bf87b58bea2f6e53
Sketch <75850871+SketchMaster2001@users.noreply.github.com>
2022-05-06 13:12:43 -0400

    Fix package.swift

--- Package.swift
+++ Package.swift
@@ -1,5 +1,14 @@
+// swift-tools-version:5.3
 import PackageDescription
 
 let package = Package(
-    name: "MimeType"
+    name: "MimeType",
+    products: [
+        .library(name: "MimeType", targets: ["MimeType"])
+    ],
+    targets: [
+        .target(
+            name: "Swiftcord"
+        )
+    ]
 )
